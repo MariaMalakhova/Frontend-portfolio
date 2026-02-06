33ccaac4cc3d7e13a20de90e9b1af779390392df
Maria Malakhova <mariamalakhova@coherentsolutions.com>
2026-02-06 10:27:21 +0100

    Initial commit

---
+++ 18 Food Order/src/components/CartItem.jsx
@@ -0,0 +1,18 @@
+import { currencyFormatter } from '../util/formatting';
+
+export default function CartItem({ item, onIncrease, onDecrease }) {
+  const { name, quantity, price } = item;
+
+  return (
+    <li className='cart-item'>
+      <p>
+        {name} - {quantity} x {currencyFormatter.format(price)}
+      </p>
+      <p className='cart-item-actions'>
+        <button onClick={onDecrease}>-</button>
+        <span>{quantity}</span>
+        <button onClick={onIncrease}>+</button>
+      </p>
+    </li>
+  );
+}
